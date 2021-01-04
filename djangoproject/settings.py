ee08fb930b04c77752b62636d6559f306e7d7608
Jefferson Lizarzabal <dvjefferson@gmail.com>
2021-01-04 11:10:12 -0300

    chore(settings): use secret key from environ

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$z#e2t2jbxz-l6_52rr$mx0azpk(2a8)1*yu-$@n+aasve_b63'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
